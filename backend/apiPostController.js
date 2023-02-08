1312a42e163eba70e8adda6b8f67e5ec05de148d
Denis <bestloveapples@gmail.com>
2023-02-09 01:39:18 +0300

    beta backend
    beta backend

--- backend/apiPostController.js
+++ backend/apiPostController.js
@@ -1,19 +1,18 @@
 import database from './database.js'
-import tools from "./tools.js";
+import tools from "./tools.js"
 
 class ApiPostController {
     async getReviews(request, response) {
-        database.query('SELECT * FROM `reviews`;', (error, rows, fields) => {
+        database.query('SELECT * FROM `reviews`', (error, rows, fields) => {
             if (error) {
-                return response.status(500).json({'error': 'Ошибка на сервере'});
+                return response.status(500).json({'error': 'Ошибка на сервере', 'code': 1})
             }
     
             response.json(rows)
-        });
+        })
     }
 
     async sendReview(request, response) {
-        console.log(request.body)
         if (
             !tools.checkJsonKey(request.body, 'author') ||
             !tools.checkJsonKey(request.body, 'image_url') ||
@@ -22,24 +21,51 @@ class ApiPostController {
             !tools.checkJsonKey(request.body, 'rating') ||
             !tools.checkJsonKey(request.body, 'key')) {
 
-            return response.status(400).json({'error': 'Некорректные данные.'});
+            return response.status(400).json({'error': 'Некорректные данные.', 'code': 2})
         }
 
-        var author = tools.delInjection(request.body.author);
-        var image_url = tools.delInjection(request.body.image_url);
-        var description = tools.delInjection(request.body.description);
-        var company = tools.delInjection(request.body.company);
-        var rating = tools.delInjection(request.body.rating);
-        var key = tools.delInjection(request.body.key);
+        let author = tools.delInjection(request.body.author)
+        let image_url = tools.delInjection(request.body.image_url)
+        let description = tools.delInjection(request.body.description)
+        let company = tools.delInjection(request.body.company)
+        let rating = tools.delInjection(request.body.rating)
+        let key = tools.delInjection(request.body.key)
 
-        database.query('SELECT * FROM `keys_for_reviews`;', (error, rows, fields) => {
+        database.query('SELECT * FROM `keys_for_reviews`', (error, rows, fields) => {
             if (error) {
-                return response.status(500).json({'error': 'Ошибка на сервере'});
+                return response.status(500).json({'error': 'Ошибка на сервере', 'code': 3})
             }
-    
-            response.json(rows)
-        });
+
+            let searched_key = false
+            
+            rows.forEach(i => {
+                if (i['key_string'] == key) {
+                    searched_key = true
+                }
+            })
+
+            if (!searched_key) {
+                return response.status(400).json({'error': 'Ключ недействителен.'})
+            }
+        })
+
+        database.query("INSERT INTO `reviews` (`author`, `image_url`, `description`, `company`, `rating`, `date`) VALUES (" +
+            "'" + author + "', " + 
+            "'" + image_url + "', " + 
+            "'" + description + "', " + 
+            "'" + company + "', " + 
+            "'" + rating + "', " + 
+            "'" + Date.now() + "'" +
+        ");", (error, rows, fields) => {
+
+            if (error) {
+                return response.status(500).json({'error': 'Ошибка на сервере', 'code': error})
+            }
+
+            response.json({'message': 'Успех.', 'items': {'id': rows.insertId}})
+
+        })
     }
 }
 
-export default new ApiPostController;
+export default new ApiPostController
