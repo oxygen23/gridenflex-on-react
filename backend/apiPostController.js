54f1e02a3fd34c77ce76d5caeb8786a551b32596
Denis <bestloveapples@gmail.com>
2023-03-06 22:10:46 +0300

    1
    1

--- backend/apiPostController.js
+++ backend/apiPostController.js
@@ -1,5 +1,6 @@
 import database from './database.js'
 import tools from "./tools.js"
+import axios from 'axios'
 
 class ApiPostController {
     async getReviews(request, response) {
@@ -59,11 +60,37 @@ class ApiPostController {
         ");", (error, rows, fields) => {
 
             if (error) {
-                return response.status(500).json({'error': 'Ошибка на сервере', 'code': error})
+                return response.status(500).json({'error': 'Ошибка на сервере', 'code': 4})
             }
 
             response.json({'message': 'Успех.', 'items': {'id': rows.insertId}})
 
+        })
+    }
+
+    async sendContacts(request, response) {
+        if (
+            !tools.checkJsonKey(request.body, 'name') ||
+            !tools.checkJsonKey(request.body, 'email') ||
+            !tools.checkJsonKey(request.body, 'contacts')) {
+
+            return response.status(400).json({'error': 'Некорректные данные.', 'code': 5})
+        }
+
+        let description = "Не указано";
+
+        if (tools.checkJsonKey(request.body, 'description')) {
+            description = tools.delInjection(request.body.description);
+        }
+
+        let name = tools.delInjection(request.body.name)
+        let email = tools.delInjection(request.body.email)
+        let contacts = tools.delInjection(request.body.contacts)
+
+        let message = `Новый заказ%0A%0AИмя отправителя: ${name}%0AПочта отправителя: ${email}%0AКонтакты отправителя: ${contacts}%0A%0AКак узнали о нас: ${description}`
+
+        axios.get(`https://api.telegram.org/bot5780555864:AAE9gBo2B9aK0PoOxrKYp0xQ9Ge6A4Pf1nI/sendMessage?chat_id=1046931046&text=${message}`).then(function (res) {
+            response.status(200).json({'message': 'Успех.'})
         })
     }
 }
--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/getReviews', ApiPostController.getReviews)
 router.post('/sendReview', ApiPostController.sendReview)
+router.post('/sendContacts', ApiPostController.sendContacts)
 
 export default router;
